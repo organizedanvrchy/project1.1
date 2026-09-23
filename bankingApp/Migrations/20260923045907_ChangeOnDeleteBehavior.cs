e41a3f2f176921c5b21ca068cd1429fa9b1f89ff
organizedanvrchy <vimalramnarain@gmail.com>
2026-09-23 01:30:50 -0400

    Add migration files for removing customer seeds and updating delete behaviors
    - Created migration `RemovedCustomerSeeds` to remove customer seed data.
    - Created migration `ChangeOnDeleteBehavior` to update the delete behavior for bank accounts.
    - Created migration `ChangeOnDeleteBehaviorForAllEntities` to apply cascading deletes for cheque requests and transactions.
    - Created migration `ResetOnDeleteBehavior` to reset foreign key constraints and their delete behaviors.

---
+++ bankingApp/Migrations/20260923045907_ChangeOnDeleteBehavior.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bankingApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeOnDeleteBehavior : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_bank_accounts_users_customer_id",
+                table: "bank_accounts");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_bank_accounts_users_customer_id",
+                table: "bank_accounts",
+                column: "customer_id",
+                principalTable: "users",
+                principalColumn: "user_id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_bank_accounts_users_customer_id",
+                table: "bank_accounts");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_bank_accounts_users_customer_id",
+                table: "bank_accounts",
+                column: "customer_id",
+                principalTable: "users",
+                principalColumn: "user_id");
+        }
+    }
+}
